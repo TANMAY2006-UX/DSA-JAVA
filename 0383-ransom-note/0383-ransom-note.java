75f888bc06c71389bb5e1ed98b2f293d050fd078
Tanmay Tajane <tanmaytajane2006@gmail.com>
2026-01-09 01:32:41 +0530

    Time: 1 ms (99.83%), Space: 46.5 MB (63.1%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,22 +1,15 @@
 class Solution {
-    public static boolean canConstruct(String ransomNote, String magazine) {
-        char[] arr = ransomNote.toCharArray();
-        char[] arr2 = magazine.toCharArray();
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < arr2.length; i++) {
-            map.put(arr2[i], map.getOrDefault(arr2[i], 0) + 1);
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] table = new int[26];
+        for(int i =0; i<magazine.length();i++){
+            table[magazine.charAt(i) - 'a']++;
         }
-        int i = 0;
-        int count = 0;
-        while(i < arr.length) {
-            if(map.containsKey(arr[i])) {
-                if(map.get(arr[i]) > 0) {
-                    map.put(arr[i], map.get(arr[i]) - 1);
-                    count++;
-                }
+        for(int i=0;i<ransomNote.length();i++){
+            table[ransomNote.charAt(i) - 'a']--;
+            if(table[ransomNote.charAt(i)-'a']<0){
+                return false;
             }
-            i++;
         }
-        return count == arr.length;
+        return true;
     }
 }
