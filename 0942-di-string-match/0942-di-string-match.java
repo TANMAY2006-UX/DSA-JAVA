90dff088251d0d195f7684c15978960700ba0f13
Tanmay Tajane <tanmaytajane2006@gmail.com>
2026-01-30 08:44:23 +0530

    Time: 2 ms (97.2%), Space: 47.4 MB (34.97%) - LeetHub

--- 0942-di-string-match/0942-di-string-match.java
+++ 0942-di-string-match/0942-di-string-match.java
@@ -1,34 +1,21 @@
 class Solution {
     public int[] diStringMatch(String s) {
         int n = s.length();
-            int[] nums = new int[n + 1];
-            
-            for (int i = 0; i <= n; i++) {
-                nums[i] = i;
-            }
+        int low = 0;
+        int high = n;
+        int[] nums = new int[n + 1];
 
-            // Look for 'D'. When you find it, reverse that segment.
-            for (int i = 0; i < n; i++) {
-                if (s.charAt(i) == 'D') {
-                    int start = i;
-                    // Find how far the 'D's go
-                    while (i < n && s.charAt(i) == 'D') {
-                        i++;
-                    }
-                    // Reverse the numbers from 'start' to 'i'
-                    reverse(nums, start, i);
-                }
+        for (int i = 0; i < n; i++) {
+            if(s.charAt(i) == 'I'){
+                nums[i] = low;
+                low++;
             }
-            return nums;
-        }
-
-        public static void reverse(int[] arr, int i, int j) {
-            while (i < j) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                i++;
-                j--;
+            else {
+                nums[i] = high;
+                high--;
             }
         }
+        nums[n] = low;
+        return nums;
     }
+}
