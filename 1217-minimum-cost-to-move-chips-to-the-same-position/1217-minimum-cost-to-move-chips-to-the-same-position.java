993b808c2407aca10a45e67fdff173da9568fee2
Tanmay Tajane <tanmaytajane2006@gmail.com>
2026-01-30 07:33:18 +0530

    Time: 0 ms (100%), Space: 42.9 MB (79.06%) - LeetHub

--- 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.java
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.java
@@ -1,21 +1,16 @@
 class Solution {
     public int minCostToMoveChips(int[] position) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int cost = 0;
+        int even = 0;
+        int odd = 0;
+
         for(int i = 0; i < position.length; i++){
-            map.put(position[i],map.getOrDefault(position[i],0)+1);
-        }
-
-        int evenChips = 0;
-        int oddChips = 0;
-
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getKey() % 2 == 0) {
-                evenChips += entry.getValue(); // Add the count of chips at this even position
-            } else {
-                oddChips += entry.getValue(); // Add the count of chips at this odd position
+            if(position[i] % 2 == 0){
+                even ++;
+            }
+            else {
+                odd++;
             }
         }
-        return Math.min(evenChips, oddChips);
+        return Math.min(even, odd);
     }
 }
