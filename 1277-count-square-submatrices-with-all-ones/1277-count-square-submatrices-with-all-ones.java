2148284bf3229463add7ab96abcdc0a006af2b64
Tanmay Tajane <tanmaytajane2006@gmail.com>
2026-02-16 14:33:07 +0530

    Time: 5 ms (99.44%), Space: 61.8 MB (61.65%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,37 +1,42 @@
 class Solution {
     int m,n;
-    int solve(int i, int j, int[][] matrix,int[][] memo){
-
-        if(i >= m || j >= n) return 0;
-
-        if(matrix[i][j] == 0) return 0;
-
-        if(memo[i][j] != -1) return memo[i][j]; 
-
-        int right = solve(i , j + 1, matrix,memo);
-        int dia = solve(i + 1, j + 1, matrix,memo);
-        int down = solve(i + 1, j, matrix,memo);
-        
-        return memo[i][j] = 1 + Math.min(right,Math.min(dia,down));
-    }
 
     public int countSquares(int[][] matrix) {
         m = matrix.length;
         n = matrix[0].length;
         int sum = 0;
-        int[][] memo = new int[m][n];
-
+        int[][] arr = new int[m][n];
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n; j++){
+                arr[i][j] = 0;
+            }
+        }
         for(int i = 0;i < m; i++){
-            for(int j = 0; j < n;j++){
-                memo[i][j] = -1;
+            arr[i][0] = matrix[i][0];
+        }
+        for(int i = 0; i < n;i++){
+            arr[0][i] = matrix[0][i];
+        }
+
+        for(int i = 1 ; i < m ; i++){
+            for(int j = 1 ; j < n; j++){
+                if(matrix[i][j] == 1)
+                    arr[i][j] = 1 + Math.min(arr[i-1][j],Math.min(arr[i-1][j-1],arr[i][j-1]));
+                else
+                    arr[i][j] = 0;
+            
             }
         }
 
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n; j++){
-                sum += solve(i, j, matrix,memo);
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j<n;j++){
+                sum += arr[i][j];
             }
         }
         return sum;
     }
 }
+
+// 1 1 1 
+// 1 1 1 
+// 1 1 1 
