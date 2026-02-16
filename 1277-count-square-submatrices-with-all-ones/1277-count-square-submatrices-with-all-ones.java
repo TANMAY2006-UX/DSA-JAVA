c75bd8766906925be6a897dfa16c68243844a212
Tanmay Tajane <tanmaytajane2006@gmail.com>
2026-02-16 19:19:03 +0530

    Time: 5 ms (99.44%), Space: 61.6 MB (88.73%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,37 +1,50 @@
 class Solution {
-    int m,n;
-    int solve(int i, int j, int[][] matrix,int[][] memo){
-
-        if(i >= m || j >= n) return 0;
-
-        if(matrix[i][j] == 0) return 0;
-
-        if(memo[i][j] != -1) return memo[i][j]; 
-
-        int right = solve(i , j + 1, matrix,memo);
-        int dia = solve(i + 1, j + 1, matrix,memo);
-        int down = solve(i + 1, j, matrix,memo);
-        
-        return memo[i][j] = 1 + Math.min(right,Math.min(dia,down));
-    }
-
     public int countSquares(int[][] matrix) {
-        m = matrix.length;
-        n = matrix[0].length;
+        int m = matrix.length; // row count
+        int n = matrix[0].length; // column count
         int sum = 0;
-        int[][] memo = new int[m][n];
+        int[][] dp = new int[m][n];
 
-        for(int i = 0;i < m; i++){
-            for(int j = 0; j < n;j++){
-                memo[i][j] = -1;
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                dp[i][j] = 0;
             }
         }
 
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n; j++){
-                sum += solve(i, j, matrix,memo);
+        for(int j = 0;j<m;j++){
+            dp[j][0] = matrix[j][0];
+        }
+        for(int j =0;j<n;j++){
+            dp[0][j] = matrix[0][j];
+        }
+
+        for(int i = 1 ; i<m; i++){
+            for(int j = 1;j<n;j++){
+                
+                if(matrix[i][j] == 0) 
+                    dp[i][j] = 0;
+                else{
+                    dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i-1][j-1],dp[i][j-1]));
+                }
+            }
+        }
+
+        for(int i = 0; i<m;i++){
+            for(int j = 0; j<n;j++){
+                sum = sum + dp[i][j];
             }
         }
         return sum;
     }
 }
+
+
+/*
+
+1 1 1 1           1  1  1  1     
+1 1 0 1           1  2  0  2
+1 1 1 1           1  2  3  3
+1 1 1 1           1  2  3  4
+
+
+*/
