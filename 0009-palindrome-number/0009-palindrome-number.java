aa851807be71ae448ade2c7fd0f7da58ae31daa4
Tanmay Tajane <tanmaytajane2006@gmail.com>
2026-02-18 16:02:33 +0530

    Time: 5 ms (76.01%), Space: 46 MB (52.98%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,25 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int[] digits = intToDigitArray(x); 
-        int i = 0;
-        int j = digits.length - 1;
-
-        while(i < j){
-            if(digits[i] != digits[j])
-                return false;
-            i++;
-            j--;
+      if (x < 0) return false;   
+        
+        int temp = x;
+        int rev = 0;
+        
+        while (temp > 0) {
+            rev = rev * 10 + temp % 10;  
+            temp = temp / 10;            
         }
-        return true;
-    }
-     private int[] intToDigitArray(int num) {
-
-        char[] chars = String.valueOf(num).toCharArray();
-
-        int[] result = new int[chars.length];
-        for (int i = 0; i < chars.length; i++) {
-            result[i] = chars[i] - '0'; 
-        }
-        return result;
+        
+        return x == rev;   
     }
 }
