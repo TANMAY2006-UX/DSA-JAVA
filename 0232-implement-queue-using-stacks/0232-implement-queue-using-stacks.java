13f01d856cc2df83dbb4886f5ca5e433c12a0f99
Tanmay Tajane <tanmaytajane2006@gmail.com>
2026-02-24 20:03:57 +0530

    Time: 0 ms (100%), Space: 42.7 MB (59.26%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,44 +1,57 @@
+// Gemini Answer But good one...
 class MyQueue {
-    Stack<Integer> s1;
-    Stack<Integer> s2;
+    Stack<Integer> input;  // s1 (Push here)
+    Stack<Integer> output; // s2 (Pop/Peek from here)
+
     public MyQueue() {
-        s1 = new Stack<>();
-        s2 = new Stack<>();
+        input = new Stack<>();
+        output = new Stack<>();
     }
     
     public void push(int x) {
-        
-        s1.push(x);
+        input.push(x);
     }
     
+    // Amortized O(1) - Lazy move
     public int pop() {
-        if(s1.empty()) return -1;
-        while(s1.size() != 1){
-            s2.push(s1.pop());
+        // If output is empty, we must refill it from input
+        if (output.empty()) {
+            transferInputToOutput();
         }
-        int rem = s1.pop();
-        while(s2.size() != 0){
-            s1.push(s2.pop());
-        }
-        return rem;
+        return output.pop();
     }
     
+    // Amortized O(1) - Lazy move
     public int peek() {
-        if(s1.empty()) return -1;
-        while(s1.size() != 1){
-            s2.push(s1.pop());
+        // Same logic: ensure output has data
+        if (output.empty()) {
+            transferInputToOutput();
         }
-        int top = s1.peek();
-        while(s2.size() != 0){
-            s1.push(s2.pop());
-        }
-        return top;
+        return output.peek();
     }
     
     public boolean empty() {
-        return (s1.empty());
+        // It's only empty if BOTH stacks are empty
+        return input.empty() && output.empty();
+    }
+
+    // Helper function to handle the "Heavy" lifting
+    private void transferInputToOutput() {
+        while (!input.empty()) {
+            output.push(input.pop());
+        }
     }
 }
+
+/*
+Your old code: Moves 1,2,3,4 to s2, pops 5... wait, actually pops 1 (if logic correct), then moves 2,3,4,5 back. A lot of work!
+
+This code: Moves 5, 4, 3, 2, 1 into output. output is now [5, 4, 3, 2, 1]. Pops 1.
+
+Now output is [5, 4, 3, 2].
+
+The next time you call pop(), you just grab 2 directly from output. Zero movement needed!
+*/
 
 /**
  * Your MyQueue object will be instantiated and called as such:
