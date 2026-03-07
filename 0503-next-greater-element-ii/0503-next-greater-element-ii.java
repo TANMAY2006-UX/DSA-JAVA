3f7fa9fd5539e7d0c459e9d35a848dd3e2a41f08
Tanmay Tajane <tanmaytajane2006@gmail.com>
2026-03-07 15:43:15 +0530

    Time: 11 ms (66.19%), Space: 48.5 MB (66.25%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -4,19 +4,14 @@ class Solution {
         int[] arr = new int[nums.length];
         int temp = 2 * nums.length;
         Arrays.fill(arr, -1);
-        
-        int j = 1;
-        if(s.isEmpty() && nums.length != 0){
-            s.push(0);
-        }
-        while(!s.isEmpty() && j < temp){
+
+        for(int j = 0 ; j < temp ; j++){
             while(!s.isEmpty() && nums[j % nums.length] > nums[s.peek()]){
                 arr[s.peek()] = nums[j  % nums.length];
                 s.pop();
             }
             if(j < nums.length)
                 s.push(j);
-            j++;
         }
         return arr;
     }
