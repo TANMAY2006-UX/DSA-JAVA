251619433f3b1dc91eea99b49b26e708da1f36f9
Tanmay Tajane <tanmaytajane2006@gmail.com>
2026-03-11 23:33:02 +0530

    Time: 8 ms (6.63%), Space: 46.3 MB (6.27%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> s = new Stack<>();
+        int n = operations.length;
+        int sum = 0;
+        for(int i = 0 ; i < n ; i++){
+            try {
+                int num = Integer.parseInt(operations[i]);
+                s.push(num);
+            } catch (NumberFormatException e) {
+                if(operations[i].equals("C")){
+                    s.pop();
+                }
+                else if(operations[i].equals("+")){
+                    int a = s.peek();
+                    s.pop();
+                    int temp = a + s.peek();
+                    s.push(a);
+                    s.push(temp);
+                }
+                else if(operations[i].equals("D")){
+                    s.push(2*s.peek());
+                }
+            }
+        }
+
+        while(!s.isEmpty()){
+            sum = sum + s.peek();
+            s.pop();
+        }
+        return sum;
+    }
+}
