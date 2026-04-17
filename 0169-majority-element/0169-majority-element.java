cd485faf1064eeb4b292cc442a85fe2f747027ca
Tanmay Tajane <tanmaytajane2006@gmail.com>
2026-04-17 10:45:41 +0530

    Time: 20 ms (11.1%), Space: 52.5 MB (91.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,27 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        if(nums.length == 1)
-            return nums[0];
-        int i = 0;
-        int j = 1;
-        int count = 0;
-        int max = 0;
-        int num = 0;
-        while(j < nums.length){
-            if(nums[i] == nums[j]){
-                count = j - i + 1;
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        int res = 0;
+        int majority = 0;
+
+        for (int n : nums) {
+            hash.put(n, 1 + hash.getOrDefault(n, 0));
+            if (hash.get(n) > majority) {
+                res = n;
+                majority = hash.get(n);
             }
-            else{
-                i = j;
-                count = 0;
-            }
-            if(max < count){
-                num = nums[i];
-                max = count;
-            }
-            j++;
         }
-        return num;
+
+        return res; 
     }
 }
