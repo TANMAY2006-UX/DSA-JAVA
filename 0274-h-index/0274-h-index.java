cc14e8438435e4a59c20989487e13edc6cb61a0b
Tanmay Tajane <tanmaytajane2006@gmail.com>
2026-04-29 15:31:38 +0530

    Time: 6 ms (25.18%), Space: 43.5 MB (59.19%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,12 +1,11 @@
 class Solution {
     public int hIndex(int[] citations) {
-        int count = 0;
         Arrays.sort(citations);
         for(int i = 0 ; i < citations.length; i++){
             if(citations[i] >= (citations.length - i)){
-                count = Math.max(count,citations.length - i);
+                return citations.length - i;
             }
         }
-        return count;
+        return 0;
     }
 }
