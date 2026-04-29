f1af83d8a020306c30174b7080fc2b8a1c14955c
Tanmay Tajane <tanmaytajane2006@gmail.com>
2026-04-29 13:18:05 +0530

    Time: 0 ms (100%), Space: 48.7 MB (51.26%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -3,27 +3,16 @@ class Solution {
         if(nums.length == 1){
             return 1;
         }
-        int i = 0;
-        int j = 1;
         int c = 0;
         int p = 0;
 
-        while(j < nums.length){
-            while(j < nums.length && nums[i] == nums[j]){
-                j++;
-            }
-            c = j - i;
-            if(c > 2){
-                c = 2;
-            }
-            while(c!=0){
-                nums[p] = nums[i];
+        for(int i = 0; i < nums.length ; i++){
+            if(p<2 || nums[i] != nums[p-2]){
+                nums[c] = nums[i];
+                c++;
                 p++;
-                c--;
             }
-            i = j;
-            //j++;
         }
-        return p;
+        return c;
     }
 }
