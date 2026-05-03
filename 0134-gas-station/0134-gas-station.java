5e5fe48ab3d89882e8e5e348c5a3ce7c08e28a6f
Tanmay Tajane <tanmaytajane2006@gmail.com>
2026-05-03 12:25:17 +0530

    Time: 2 ms (99.32%), Space: 119.6 MB (25.25%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,53 +1,28 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int start = -1;
-        int sumg = 0;
-        int sumc = 0;
-        int i = 0;
-        int tank = 0;
-        int count = 0;
-        while(i < gas.length){
-            sumg += gas[i];
-            i++;
+        int totalGas = 0;
+        int totalCost = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
         }
-        i = 0;
-        while(i < cost.length){
-            sumc += cost[i];
-            i++;
+        if (totalGas < totalCost) {
+            return -1;
         }
 
-        if(sumg >= sumc){
-            if(gas.length == 1){
-            return 0;
-        }
-            int j = 0;
-            while(j < gas.length) {
-                if (gas[j] > cost[j]){
-                    start = j;
-                    tank = tank + gas[j];
-                }
-                int temp = j;
-                while (count != gas.length && tank > 0) {
-                    tank = tank - cost[temp];
-                    temp = (temp + 1) % gas.length;
-                    if(tank >= 0) {
-                        tank += gas[temp];
-                        count++;
-                    }
-                    else{
-                        count = 0;
-                        tank = 0;
-                    }
-                }
-                if (count == gas.length) {
-                    return start;
-                }
-                j++;
+        int currentTank = 0;
+        int startingStation = 0;
+        
+        for (int i = 0; i < gas.length; i++) {
+            currentTank += gas[i] - cost[i];
+
+            if (currentTank < 0) {
+                startingStation = i + 1;
+                currentTank = 0; 
             }
         }
-        else{
-            return start;
-        }
-        return start;
+        
+        return startingStation;
     }
 }
