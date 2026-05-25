1dda8938c82fe35eefcaad82bc89780370d9d4e2
Tanmay Tajane <tanmaytajane2006@gmail.com>
2026-05-25 08:35:27 +0530

    Time: 11 ms (22.62%), Space: 47.3 MB (7.58%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0 ; i < nums.length - 1 ; i++){
+            if(nums[i] == nums[i + 1]){
+                i = i + 1;
+            }
+            else{
+                return nums[i];
+            }
+        }
+        return nums[nums.length - 1];
+    }
+}
